db8a559a1cb3896d6741422be035b736c8b5f73b
Arthur923 <54887865+Arthur923@users.noreply.github.com>
2020-04-22 16:15:35 +0300

    Update telegram-bot.js

--- telegram-bot.js
+++ telegram-bot.js
@@ -1,25 +1,21 @@
 import TelegramBot from 'node-telegram-bot-api';
-
 const token = '1147541859:AAEdivgSKW97qWA_v-Vn38eXwESf1qnt7x4';
 const bot = new TelegramBot(token, {polling: true});
-
 const notes = [];
-
-bot.onText(/напиши (.+) в (.+)/, (msg, match) => {
+bot.onText(/Wrtieln (.+) в (.+)/, (msg, match) => {
     const userId = msg.from.id;
     const text = match[1];
     const time = match[2];
-
     notes.push({ 'uid': userId, 'time': time, 'text': text });
 
-    bot.sendMessage(userId, 'Ойойо! Я напишу про це, тільки кинь 100 грн на карту :)');
+    bot.sendMessage(userId, 'Fail :)');
 });
 
 setInterval(() => {
     for (let i = 0; i < notes.length; i++) {
     const curDate = `${new Date().getHours()}:${new Date().getMinutes()}`;
     if (notes[i]['time'] === curDate) {
-      bot.sendMessage(notes[i]['uid'], `Пишу вам, ви повинні: ${notes[i]['text']} щас.`);
+      bot.sendMessage(notes[i]['uid'], `Have to: ${notes[i]['text']} now.`);
       notes.splice(i, 1);
     }
   }
